8267ead85c0d667b5f8b823a2f39a7ac84aa1119
Sohmteee <sohmtee@gmail.com>
2022-09-10 19:02:02 +0100

    commit

--- lib/splash screen 2.dart
+++ lib/splash screen 2.dart
@@ -9,37 +9,35 @@ class SplashScreen2 extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.white,
-        body: Center(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16.w),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Image.asset("assets/images/splash_screen_pic.png"),
-                SizedBox(height: 15.h),
-                Text(
-                  "To the hub",
-                  style: TextStyle(
-                    fontSize: 18.sp,
-                    fontWeight: FontWeight.w700,
-                  ),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.w),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset("assets/images/splash_screen_pic.png"),
+              SizedBox(height: 15.h),
+              Text(
+                "To the hub",
+                style: TextStyle(
+                  fontSize: 18.sp,
+                  fontWeight: FontWeight.w700,
                 ),
-                SizedBox(height: 24.h),
-                Text(
-                  "Quis porttitor nullam mi in tellus quis commodo. Mattis dignissim curabitur sed cursus sed porttitor vel at iaculis. Eget varius posuere massa, amet eu, non libero. Laoreet arcu sem libero, non amet, erat risus eu, egestas. Augue nulla feugiat nulla vel. Aliquet non dictum amet fermentum nibh enim fames mauris praesent. Pulvinar augue semper egestas praesent tincidunt quis eu.",
-                  style:
-                      TextStyle(fontSize: 18.sp, fontWeight: FontWeight.w400),
+              ),
+              SizedBox(height: 24.h),
+              Text(
+                "Quis porttitor nullam mi in tellus quis commodo. Mattis dignissim curabitur sed cursus sed porttitor vel at iaculis. Eget varius posuere massa, amet eu, non libero. Laoreet arcu sem libero, non amet, erat risus eu, egestas. Augue nulla feugiat nulla vel. Aliquet non dictum amet fermentum nibh enim fames mauris praesent. Pulvinar augue semper egestas praesent tincidunt quis eu.",
+                style: TextStyle(fontSize: 18.sp, fontWeight: FontWeight.w400),
+              ),
+              SizedBox(height: 87.h),
+              Text(
+                "Begin your journey",
+                style: TextStyle(
+                  fontSize: 18.sp,
+                  fontWeight: FontWeight.w400,
                 ),
-                SizedBox(height: 87.h),
-                Text(
-                  "Begin your journey",
-                  style: TextStyle(
-                    fontSize: 18.sp,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
