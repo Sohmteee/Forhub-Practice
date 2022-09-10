86d93a39f7d0e197a8ff5b68251c0d25353c58ff
Sohmteee <sohmtee@gmail.com>
2022-09-10 19:34:02 +0100

    commit

--- lib/onboarding screen/onboarding screen 1.dart
+++ lib/onboarding screen/onboarding screen 1.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:forhub/widgets/onboarding.dart';
 
 class OnBoardingScreen1 extends StatelessWidget {
   const OnBoardingScreen1({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    
+    return OnBoarding(
+      title: "Quick and easy way to locate a hub",
+      image: "assets/images/on1.png",
+    );
   }
 }
