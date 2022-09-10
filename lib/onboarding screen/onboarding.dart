ea42fa167465d962001af23cd1c9f438525274bc
Sohmteee <sohmtee@gmail.com>
2022-09-10 19:13:40 +0100

    commit

---
+++ lib/onboarding screen/onboarding.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class OnBoarding extends StatelessWidget {
+  const OnBoarding({Key? key}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {}
+}
