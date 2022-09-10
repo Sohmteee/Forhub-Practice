8e9fd3217e65effcd960a3030bc8e16bc72be830
Sohmteee <sohmtee@gmail.com>
2022-09-10 19:14:44 +0100

    commit

--- lib/onboarding screen/onboarding.dart
+++ lib/onboarding screen/onboarding.dart
@@ -1,10 +1,15 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 class OnBoarding extends StatelessWidget {
-  const OnBoarding({Key? key}) : super(key: key);
+  const OnBoarding({
+    Key? key,
+    required title,
+  }) : super(key: key);
 
-  final String title;
+  final String title = "";
+  final Image.a
 
   @override
   Widget build(BuildContext context) {}
