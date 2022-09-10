2a21950655a70ed73a51c6b25e75f2b31a6b9d9a
Sohmteee <sohmtee@gmail.com>
2022-09-10 20:37:48 +0100

    commit

--- lib/onboarding screen/onboarding screen.dart
+++ lib/onboarding screen/onboarding screen.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_onboarding_slider/flutter_onboarding_slider.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    ret
+    return OnBoardingSlider(
+      totalPage: totalPage,
+      headerBackgroundColor: Colors.white,
+      background: ,
+      speed: speed,
+      pageBodies: pageBodies,
+    );
   }
 }
