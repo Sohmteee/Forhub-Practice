d3dc2e60fac2bd51098235755aea27c01e04e1c9
Sohmteee <sohmtee@gmail.com>
2022-09-10 20:40:58 +0100

    commit

--- lib/public data.dart
+++ lib/public data.dart
@@ -1,3 +1,11 @@
 import 'package:forhub/widgets/onboarding.dart';
 
-OnBoarding 
+OnBoarding onBoarding1 = const OnBoarding(
+  title: "Quick and easy way to locate a hub",
+  image: "assets/images/on1.png",
+);
+
+OnBoarding onBoarding3 = const OnBoarding(
+  title: "Quick and easy",
+  image: "assets/images/on3.png",
+);
