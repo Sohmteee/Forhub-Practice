d47827dff1308692baedf43bea83b9d68fe3cf97
Sohmteee <sohmtee@gmail.com>
2022-09-10 19:11:33 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import 'splash screen 2.dart';
-import 'splash screen.dart';
+import 'splash screens/splash screen 2.dart';
+import 'splash screens/splash screen.dart';
 
 void main() {
   runApp(const MyApp());
