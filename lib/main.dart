e71c97e6ea55ff16dc722c8ac5a78561bb6ddb61
Sohmteee <sohmtee@gmail.com>
2022-09-10 17:35:22 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'splash_screen.dart';
+import 'splash screen.dart';
 
 void main() {
   runApp(const MyApp());
---
+++ lib/splash screen 2.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen2 extends StatelessWidget {
+  const SplashScreen2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+          body:),
+    );
+  }
+}
