81376b7b92f324664c4603f3bf9c812ac601635a
Sohmteee <sohmtee@gmail.com>
2022-09-10 17:34:18 +0100

    commit

---
+++ lib/splash_screen copy.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+          body: Center(
+        child: Image.asset("assets/images/FORHUB 1.png"),
+      )),
+    );
+  }
+}
