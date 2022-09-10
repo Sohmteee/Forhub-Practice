97b4584425c4b5f8e564d37434e213c4795c96a6
Sohmteee <sohmtee@gmail.com>
2022-09-10 17:48:16 +0100

    commit

--- lib/splash screen 2.dart
+++ lib/splash screen 2.dart
@@ -9,23 +9,26 @@ class SplashScreen2 extends StatelessWidget {
       child: Scaffold(
         backgroundColor: Colors.white,
         body: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Image.asset("assets/images/splash_screen_pic.png"),
-              const Text(
-                "To the hub",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset("assets/images/splash_screen_pic.png"),
+                const Text(
+                  "To the hub",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              const Text(
-                "To the hub",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
+                const Text(
+                  "Quis porttitor nullam mi in tellus quis commodo. Mattis dignissim curabitur sed cursus sed porttitor vel at iaculis. Eget varius posuere massa, amet eu, non libero. Laoreet arcu sem libero, non amet, erat risus eu, egestas. Augue nulla feugiat nulla vel. Aliquet non dictum amet fermentum nibh enim fames mauris praesent. Pulvinar augue semper egestas praesent tincidunt quis eu.",
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
