58f039edbb600961e9b5927d49143795fe2dba55
Sohmteee <sohmtee@gmail.com>
2022-09-10 18:34:38 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: const Size(width, height),
+      designSize: const Size(375, 812),
       builder: (_, child)=> MaterialApp(
         title: 'For Hub',
         theme: ThemeData(
