1d32035b3426b9009a715d1c2e9f8b52c255660e
Sohmteee <sohmtee@gmail.com>
2022-09-11 15:31:19 +0100

    commit

--- lib/public data.dart
+++ lib/public data.dart
@@ -1,6 +1,16 @@
 import 'package:forhub/widgets/onboarding.dart';
 
-List<String> 
+List<String> titles = [
+  "Quick and easy way to locate a hub",
+  "Book for hub at your own convinent time",
+  "Quick and easy",
+];
+
+List<String> images = [
+  "assets/images/on1.png",
+  "assets/images/on2.png",
+  "assets/images/on3.png",
+];
 
 OnBoarding onBoarding1 = const OnBoarding(
   title: "Quick and easy way to locate a hub",
