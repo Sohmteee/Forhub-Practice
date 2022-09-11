c69b20c4c26f160e45e76fe7e83c850d68324d88
Sohmteee <sohmtee@gmail.com>
2022-09-11 17:49:18 +0100

    commit

--- lib/splash screens/splash screen 1.dart
+++ lib/splash screens/splash screen 1.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -8,7 +9,7 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const SafeArea(
-      child: EasySplashScreen(
+      child: SplashScreen(
         
       ),
     );
