890461502a49eeea39319861fba698a86c56515a
Sohmteee <sohmtee@gmail.com>
2022-09-11 17:58:51 +0100

    commit

--- lib/splash screens/splash screen 1.dart
+++ lib/splash screens/splash screen 1.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -9,7 +11,20 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+    Timer(
+      const Duration(seconds: 3),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    
+    return SafeArea(
+      child: Scaffold(
+          body: Center(
+        child: Image.asset("assets/images/FORHUB 1.png"),
+      )),
+    );
   }
 }
