fbd20228515bcccbd0510c567b990202606f8a61
Sohmteee <sohmtee@gmail.com>
2022-09-11 17:57:47 +0100

    commit

--- lib/splash screens/splash screen 1.dart
+++ lib/splash screens/splash screen 1.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-          body: Center(
-        child: Image.asset("assets/images/FORHUB 1.png"),
-      )),
-    );
+    
   }
 }
