b28151ced7f902ed35935b3cf3fe8dd8c5361aec
Sohmteee <sohmtee@gmail.com>
2022-09-11 17:52:30 +0100

    commit

--- lib/splash screens/splash screen 1.dart
+++ lib/splash screens/splash screen 1.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:forhub/onboarding%20screen/onboarding_screen.dart';
+// ignore: import_of_legacy_library_into_null_safe
 import 'package:splashscreen/splashscreen.dart';
 
 class SplashScrn extends StatelessWidget {
@@ -10,6 +12,7 @@ class SplashScrn extends StatelessWidget {
       child: SplashScreen(
         backgroundColor: Colors.white,
         image: Image.asset("assets/images/FORHUB 1.png"),
+        navigateAfterSeconds: const OnBoardingScreen(),
       ),
     );
   }
