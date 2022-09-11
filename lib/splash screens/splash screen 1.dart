ee17e4639493f76d61fc4804892608519b724db9
Sohmteee <sohmtee@gmail.com>
2022-09-11 17:55:40 +0100

    commit

--- lib/splash screens/splash screen 1.dart
+++ lib/splash screens/splash screen 1.dart
@@ -1,18 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:forhub/onboarding%20screen/onboarding_screen.dart';
-// ignore: import_of_legacy_library_into_null_safe
-import 'package:splashscreen/splashscreen.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import '';
 
-class SplashScrn extends StatelessWidget {
-  const SplashScrn({Key? key}) : super(key: key);
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
+    return const SafeArea(
       child: SplashScreen(
-        backgroundColor: Colors.white,
-        image: Image.asset("assets/images/FORHUB 1.png"),
-        navigateAfterSeconds: const OnBoardingScreen(),
+        
       ),
     );
   }
