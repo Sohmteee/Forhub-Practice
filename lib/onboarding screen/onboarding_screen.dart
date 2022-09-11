5bc4aaf8ec17dcfbe961cfe3a31c0fa81924acc1
Sohmteee <sohmtee@gmail.com>
2022-09-11 12:33:30 +0100

    commit

--- lib/onboarding screen/onboarding_screen.dart
+++ lib/onboarding screen/onboarding_screen.dart
@@ -2,22 +2,20 @@ import 'package:appinio_swiper/appinio_swiper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_onboarding_slider/flutter_onboarding_slider.dart';
 import 'package:forhub/public%20data.dart';
+import 'package:forhub/widgets/onboarding.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    List pages = [
+    List pages = <OnBoarding>[
       onBoarding1, onBoarding2, onBoarding3,
     ];
 
     return Scaffold(
       body: PageView(
-        itemCount: 3,
-        itemBuilder: (_, index) {
-          
-        },
+        children: pages,
       ),
     );
   }
