acf8d4cf2d81ce897b8d10c800047d6fc1ea01d9
Sohmteee <sohmtee@gmail.com>
2022-09-12 11:05:12 +0100

    commit

--- lib/widgets/onboarding.dart
+++ lib/widgets/onboarding.dart
@@ -1,6 +1,58 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+class OnBoarding extends StatefulWidget {
+  const OnBoarding({Key? key,
+    required this.title,
+    required this.image,}) : super(key: key);
+
+    final String title,
+      image,
+      writeUp =
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non ipsum tempor auctor ipsum sollicitudin dui vel eu mi";
+
+
+  @override
+  State<OnBoarding> createState() => _OnBoardingState();
+}
+
+class _OnBoardingState extends State<OnBoarding> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(
+        left: 18.w,
+        right: 27.w,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            title,
+            style: TextStyle(
+              fontFamily: "Sen",
+              fontWeight: FontWeight.w700,
+              fontSize: 24.sp,
+            ),
+            textAlign: TextAlign.start,
+          ),
+          SizedBox(height: 50.h),
+          Image.asset(image),
+          SizedBox(height: 40.h),
+          Text(
+            writeUp,
+            style: TextStyle(
+              fontFamily: "Sen",
+              fontWeight: FontWeight.w400,
+              fontSize: 18.sp,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 class OnBoarding extends StatelessWidget {
   const OnBoarding({
     Key? key,
