43961a3081f9fe716d4f333ced2787d39e577aa9
Sohmteee <sohmtee@gmail.com>
2022-09-12 10:48:08 +0100

    commit

---
+++ lib/widgets/onboarding_circle.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+Widget OnBoardingCircle = Container(
+  height: 8.sp,
+  width: 8.sp,
+  decoration: BoxDecoration(
+    color: Colors.orange,
+    borderRadius: BorderRadius.circular(20),
+  ),
+);
