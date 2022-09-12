e2c9a8e241d3055e98d019f57989b25c47663bb9
Sohmteee <sohmtee@gmail.com>
2022-09-12 11:03:12 +0100

    commit

--- lib/onboarding screen/onboarding_screen.dart
+++ lib/onboarding screen/onboarding_screen.dart
@@ -19,45 +19,52 @@ class OnBoardingScreen extends StatelessWidget {
           SizedBox(
             height: MediaQuery.of(context).size.height * .8,
             child: PageView.builder(
-              controller: controller,
-              itemCount: 3,
-              itemBuilder: (_, index) {
-                return OnBoarding(
-                  title: titles[index],
-                  image: images[index],
-                );
-              },
-              onPageChanged: (index) {
-                switch (index) {
-                  case 0:
-                    on1 = true;
-                    on2 = false;
-                    on3 = false;
-                    break;
-                  case 1:
-                    on1 = false;
-                    on2 = true;
-                    on3 = false;
-                    break;
-                  case 2:
-                    on1 = true;
-                    on2 = false;
-                    on3 = false;
-                    break;
-                }
-              }),
-            ),
+                controller: controller,
+                itemCount: 3,
+                itemBuilder: (_, index) {
+                  return OnBoarding(
+                    title: titles[index],
+                    image: images[index],
+                  );
+                },
+                onPageChanged: (index) {
+                  switch (index) {
+                    case 0:
+                      on1 = true;
+                      on2 = false;
+                      on3 = false;
+                      break;
+
+                    case 1:
+                      on1 = false;
+                      on2 = true;
+                      on3 = false;
+                      break;
+
+                    case 2:
+                      on1 = false;
+                      on2 = false;
+                      on3 = true;
+                      break;
+                  }
+                }),
           ),
           Expanded(
             child: SizedBox(
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  OnBoardingCircle(active: on1,),
+                  OnBoardingCircle(
+                    active: on1,
+                  ),
                   SizedBox(width: 8.w),
-                  OnBoardingCircle(active: on2,),
+                  OnBoardingCircle(
+                    active: on2,
+                  ),
                   SizedBox(width: 8.w),
-                  OnBoardingCircle(active: on3,),
+                  OnBoardingCircle(
+                    active: on3,
+                  ),
                   SizedBox(width: 8.w),
                 ],
               ),
